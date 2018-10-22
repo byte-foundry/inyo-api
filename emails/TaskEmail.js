48105daaf7a432bcfa1e2f5a65a08696343347ce
FranzPoize <francois.poizat@gmail.com>
2018-10-22 16:46:43 +0200

    added item name to task validation email

--- emails/TaskEmail.js
+++ emails/TaskEmail.js
@@ -1,7 +1,7 @@
 const sendEmail = require('./SendEmail.js');
 
-async function sendTaskValidationEmail({email, user, customerName, projectName}) {
-  return sendEmail(email, {user, customerName, projectName}, 'd-83233d7427a642a9a9218f3f7d7db5e0');
+async function sendTaskValidationEmail({email, user, customerName, itemName, projectName}) {
+  return sendEmail(email, {user, customerName, projectName, itemName}, 'd-83233d7427a642a9a9218f3f7d7db5e0');
 }
 
 module.exports = {
