ee586d4b2b97a4f9114a2d1b0cec46d185f9dd23
FranzPoize <francois.poizat@gmail.com>
2018-10-25 18:00:53 +0200

    correcting unexpected token

--- emails/TaskEmail.js
+++ emails/TaskEmail.js
@@ -1,7 +1,7 @@
 const sendEmail = require('./SendEmail.js');
 
 async function sendTaskValidationEmail({email, user, customerName, itemName, projectName, sections, quoteUrl}) {
-  return sendEmail({email, data: {user, customerName, projectName, itemName, sections, quoteUrl}, 'd-83233d7427a642a9a9218f3f7d7db5e0'});
+  return sendEmail({email, data: {user, customerName, projectName, itemName, sections, quoteUrl}, templateId: 'd-83233d7427a642a9a9218f3f7d7db5e0'});
 }
 
 module.exports = {
