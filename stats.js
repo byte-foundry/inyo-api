546925faa1394af37016febd0ed8c56e6adfaf53
Alexis Launay <yorunohikage@gmail.com>
2018-11-07 11:33:14 +0100

    Add node-fetch and remove stats from dev

--- stats.js
+++ stats.js
@@ -1,16 +1,24 @@
-const {DATADOG_API_KEY} = process.env;
+const fetch = require('node-fetch');
+
+const {NODE_ENV, DATADOG_API_KEY} = process.env;
 
 async function sendMetric({metric, count = 1}) {
+	if (NODE_ENV !== 'production') {
+		return {};
+	}
+
 	const response = await fetch(
 		`https://api.datadoghq.com/api/v1/series?api_key=${DATADOG_API_KEY}`,
 		{
 			method: 'POST',
 			body: JSON.stringify({
-				series: [{
-					metric,
-					type: 'count',
-					points: [[Math.floor(Date.now() / 1000), count]],
-				}],
+				series: [
+					{
+						metric,
+						type: 'count',
+						points: [[Math.floor(Date.now() / 1000), count]],
+					},
+				],
 			}),
 		},
 	);
