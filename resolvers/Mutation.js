094c0db0ca22744e68a74fb85b91b7e2cd569dd6
Alexis Launay <yorunohikage@gmail.com>
2018-11-08 18:06:55 +0100

    Add updatePassword mutation

--- resolvers/Mutation.js
+++ resolvers/Mutation.js
@@ -91,6 +91,22 @@ const Mutation = {
 			user,
 		};
 	},
+	updatePassword: async (parent, {oldPassword, newPassword}, ctx) => {
+		const user = await ctx.db.user({id: getUserId(ctx)});
+
+		const valid = await compare(oldPassword, user.password);
+
+		if (!valid) {
+			throw new Error('Invalid password');
+		}
+
+		const hashedPassword = await hash(newPassword, 10);
+
+		return ctx.db.updateUser({
+			where: {id: user.id},
+			data: {password: hashedPassword},
+		});
+	},
 	updateUser: async (
 		parent,
 		{
