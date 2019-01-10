ac3f80a5b2148c495751c251a3e73e9e2c2662b6
Alexis Launay <yorunohikage@gmail.com>
2019-01-10 12:10:25 +0100

    Add position to updateItem

--- resolvers/__tests__/updateItem.js
+++ resolvers/__tests__/updateItem.js
@@ -27,13 +27,16 @@ describe('updateItem', () => {
 				items: () => ({
 					$fragment: () => [
 						{
+							id: 'item-id',
 							name: 'name',
 							status: 'PENDING',
 							description: 'description',
 							unit: 2,
 							reviewer: 'USER',
+							position: 0,
 							section: {
 								id: 'section-id',
+								items: [{id: 'item-id', position: 0}],
 								project: {
 									id: 'project-id',
 									status: 'ONGOING',
@@ -63,6 +66,166 @@ describe('updateItem', () => {
 		};
 
 		const item = await updateItem({}, args, ctx);
+
+		expect(item).toMatchObject(args);
+	});
+
+	it("should let a user change backward an item's position", async () => {
+		const args = {
+			id: 'item-4',
+			position: 2,
+		};
+		const ctx = {
+			request: {
+				get: () => 'user-token',
+			},
+			db: {
+				user: () => ({
+					id: 'user-id',
+					firstName: 'Jean',
+					lastName: 'Michel',
+				}),
+				items: () => ({
+					$fragment: () => [
+						{
+							id: 'item-4',
+							name: 'name',
+							status: 'PENDING',
+							description: 'description',
+							unit: 2,
+							reviewer: 'USER',
+							position: 5,
+							section: {
+								id: 'section-id',
+								items: [
+									{id: 'item-0', position: 0},
+									{id: 'item-1', position: 1},
+									{id: 'item-2', position: 2},
+									{id: 'item-3', position: 3},
+									{id: 'item-4', position: 4},
+								],
+								project: {
+									id: 'project-id',
+									status: 'ONGOING',
+									name: "C'est notre projeeet",
+									customer: {
+										title: 'MONSIEUR',
+										firstName: 'Jean',
+										lastName: 'Michel',
+										email: 'jean@michel.org',
+										serviceCompany: {
+											owner: {
+												firstName: 'Adrien',
+												lastName: 'David',
+											},
+										},
+									},
+								},
+							},
+						},
+					],
+				}),
+				updateItem: jest.fn(({where, data}) => ({
+					id: where.id,
+					...data,
+				})),
+			},
+		};
+
+		const item = await updateItem({}, args, ctx);
+
+		expect(ctx.db.updateItem).toHaveBeenNthCalledWith(1, {
+			where: {id: 'item-2'},
+			data: {position: 3},
+		});
+		expect(ctx.db.updateItem).toHaveBeenNthCalledWith(2, {
+			where: {id: 'item-3'},
+			data: {position: 4},
+		});
+		expect(ctx.db.updateItem).toHaveBeenNthCalledWith(3, {
+			where: {id: 'item-4'},
+			data: expect.objectContaining({position: 2}),
+		});
+
+		expect(item).toMatchObject(args);
+	});
+
+	it("should let a user change forward an item's position", async () => {
+		const args = {
+			id: 'item-1',
+			position: 3,
+		};
+		const ctx = {
+			request: {
+				get: () => 'user-token',
+			},
+			db: {
+				user: () => ({
+					id: 'user-id',
+					firstName: 'Jean',
+					lastName: 'Michel',
+				}),
+				items: () => ({
+					$fragment: () => [
+						{
+							id: 'item-1',
+							name: 'name',
+							status: 'PENDING',
+							description: 'description',
+							unit: 2,
+							reviewer: 'USER',
+							position: 1,
+							section: {
+								id: 'section-id',
+								items: [
+									{id: 'item-0', position: 0},
+									{id: 'item-1', position: 1},
+									{id: 'item-2', position: 2},
+									{id: 'item-3', position: 3},
+									{id: 'item-4', position: 4},
+								],
+								project: {
+									id: 'project-id',
+									status: 'ONGOING',
+									name: "C'est notre projeeet",
+									customer: {
+										title: 'MONSIEUR',
+										firstName: 'Jean',
+										lastName: 'Michel',
+										email: 'jean@michel.org',
+										serviceCompany: {
+											owner: {
+												firstName: 'Adrien',
+												lastName: 'David',
+											},
+										},
+									},
+								},
+							},
+						},
+					],
+				}),
+				updateItem: jest.fn(({where, data}) => ({
+					id: where.id,
+					...data,
+				})),
+			},
+		};
+
+		const item = await updateItem({}, args, ctx);
+
+		expect(ctx.db.updateItem).toHaveBeenNthCalledWith(1, {
+			where: {id: 'item-2'},
+			data: {position: 1},
+		});
+		expect(ctx.db.updateItem).toHaveBeenNthCalledWith(2, {
+			where: {id: 'item-3'},
+			data: {position: 2},
+		});
+		expect(ctx.db.updateItem).toHaveBeenNthCalledWith(3, {
+			where: {id: 'item-1'},
+			data: expect.objectContaining({position: 3}),
+		});
 
 		expect(item).toMatchObject(args);
 	});
--- resolvers/updateItem.js
+++ resolvers/updateItem.js
@@ -22,6 +22,7 @@ const updateItem = async (
 		reviewer,
 		comment,
 		notifyCustomer = true,
+		position: wantedPosition,
 		token,
 	},
 	ctx,
@@ -88,7 +89,12 @@ const updateItem = async (
 	}).$fragment(gql`
 		fragment ItemWithQuoteAndProject on Item {
 			status
+			position
 			section {
+				items(orderBy: position_ASC) {
+					id
+					position
+				}
 				option {
 					quote {
 						status
@@ -122,6 +128,46 @@ const updateItem = async (
 			);
 		}
 
+		let position;
+		const initialPosition = item.section.items.findIndex(
+			sectionItem => sectionItem.id === item.id,
+		);
+
+		if (initialPosition === -1) {
+			throw new Error(
+				`Item '${item.id}' has not been found in Section '${
+					item.section.id
+				}' items.`,
+			);
+		}
+
+		if (wantedPosition && wantedPosition !== initialPosition) {
+			if (wantedPosition < 0) {
+				position = 0;
+			}
+			else if (wantedPosition > item.section.items.length) {
+				position = item.section.items.length;
+			}
+			else {
+				position = wantedPosition;
+			}
+
+			const itemsToUpdate
+				= wantedPosition > initialPosition
+					? item.section.items.slice(initialPosition + 1, wantedPosition + 1)
+					: item.section.items.slice(wantedPosition, initialPosition);
+
+			const startIndex
+				= wantedPosition > initialPosition ? initialPosition : wantedPosition + 1;
+
+			await Promise.all(
+				itemsToUpdate.map((sectionItem, index) => ctx.db.updateItem({
+					where: {id: sectionItem.id},
+					data: {position: startIndex + index},
+				})),
+			);
+		}
+
 		const updatedItem = await ctx.db.updateItem({
 			where: {id},
 			data: {
@@ -131,6 +177,7 @@ const updateItem = async (
 				unit,
 				status: 'PENDING',
 				reviewer,
+				position,
 				comments: {
 					create: comment && {
 						text: comment.text,
