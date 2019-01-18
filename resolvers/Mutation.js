be801ed624d4b4422bd6ae042d97e6b11a78f039
FranzPoize <francois.poizat@gmail.com>
2019-01-18 17:38:05 +0100

    added update customer mutation

--- resolvers/Mutation.js
+++ resolvers/Mutation.js
@@ -204,6 +204,12 @@ const Mutation = {
 			},
 		});
 	},
+	updateCustomer: async (parent, {id, customer}, ctx) => ctx.db.updateCustomer({
+		where: {
+			id,
+		},
+		data: customer,
+	}),
 	createProject,
 	updateProject,
 	finishProject,
