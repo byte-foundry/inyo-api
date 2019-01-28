5444ea7dcd9589e84b6e920e63898c4863de20ba
Alexis Launay <yorunohikage@gmail.com>
2019-01-28 17:27:29 +0100

    Add section reordering

--- resolvers/Mutation.js
+++ resolvers/Mutation.js
@@ -411,7 +411,7 @@ const Mutation = {
 			items: {create: items},
 		});
 	},
-	updateSection: async (parent, {id, name}, ctx) => {
+	updateSection: async (parent, {id, name, position: wantedPosition}, ctx) => {
 		const [section] = await ctx.db.sections({
 			where: {
 				id,
@@ -442,15 +442,80 @@ const Mutation = {
 					},
 				],
 			},
-		});
+		}).$fragment(gql`
+			fragment sectionWithProject on Section {
+				id
+				project {
+					sections(orderBy: position_ASC) {
+						id
+					}
+				}
+			}
+		`);
 
 		if (!section) {
 			throw new NotFoundError(`Section '${id}' has not been found.`);
 		}
 
+		const {project} = section;
+		let position;
+		const initialPosition = project.findIndex(
+			projectSection => projectSection.id === section.id,
+		);
+
+		if (initialPosition === -1) {
+			throw new Error(
+				`Section '${section.id}' has not been found in Project '${
+					section.project.id
+				}' sections.`,
+			);
+		}
+
+		if (
+			typeof wantedPosition === 'number'
+			&& wantedPosition !== initialPosition
+		) {
+			if (wantedPosition < 0) {
+				position = 0;
+			}
+			else if (wantedPosition > project.sections.length) {
+				position = project.sections.length;
+			}
+			else {
+				position = wantedPosition;
+			}
+
+			// TODO: externalize
+			const reorderProject = async (
+				sections,
+				initialPosition, // eslint-disable-line no-shadow
+				wantedPosition, // eslint-disable-line no-shadow
+				ctx, // eslint-disable-line no-shadow
+			) => {
+				const itemsToUpdate
+					= wantedPosition > initialPosition
+						? sections.slice(initialPosition + 1, wantedPosition + 1)
+						: sections.slice(wantedPosition, initialPosition);
+
+				const startIndex
+					= wantedPosition > initialPosition
+						? initialPosition
+						: wantedPosition + 1;
+
+				await Promise.all(
+					itemsToUpdate.map((sectionItem, index) => ctx.db.updateSection({
+						where: {id: sectionItem.id},
+						data: {position: startIndex + index},
+					})),
+				);
+			};
+
+			reorderProject(project.sections, initialPosition, position, ctx);
+		}
+
 		return ctx.db.updateSection({
 			where: {id},
-			data: {name},
+			data: {name, position},
 		});
 	},
 	removeSection: async (parent, {id}, ctx) => {
