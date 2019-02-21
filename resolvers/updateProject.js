54f5d6fca66ee44e948b5bea8674c493af6c893e
Alexis Launay <yorunohikage@gmail.com>
2019-02-21 17:02:49 +0100

    Add customer to updateProject

--- resolvers/updateProject.js
+++ resolvers/updateProject.js
@@ -4,17 +4,18 @@ const {NotFoundError} = require('../errors');
 const updateProject = async (
 	parent,
 	{
-		id, name, deadline, notifyActivityToCustomer,
+		id, name, customerId, customer, deadline, notifyActivityToCustomer,
 	},
 	ctx,
 ) => {
+	const userId = getUserId(ctx);
 	const [project] = await ctx.db.projects({
 		where: {
 			id,
 			customer: {
 				serviceCompany: {
 					owner: {
-						id: getUserId(ctx),
+						id: userId,
 					},
 				},
 			},
@@ -36,9 +37,31 @@ const updateProject = async (
 		});
 	}
 
+	const variables = {};
+
+	if (customerId) {
+		variables.customer = {
+			connect: {id: customerId},
+		};
+	}
+	else if (customer) {
+		const userCompany = await ctx.db.user({id: userId}).company();
+
+		variables.customer = {
+			create: {
+				...customer,
+				serviceCompany: {connect: {id: userCompany.id}},
+				address: {
+					create: customer.address,
+				},
+			},
+		};
+	}
+
 	return ctx.db.updateProject({
 		where: {id},
 		data: {
+			...variables,
 			name,
 			deadline,
 			notifyActivityToCustomer,
