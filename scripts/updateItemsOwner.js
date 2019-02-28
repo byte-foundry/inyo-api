26f504fdb60fba47886fe0ee1ed3f21509c22f81
Alexis Launay <yorunohikage@gmail.com>
2019-02-28 17:11:36 +0100

    Add scripts to update projects and items owner

---
+++ scripts/updateItemsOwner.js
@@ -0,0 +1,51 @@
+const {prisma} = require('../generated/prisma-client');
+
+const gql = String.raw;
+
+function sleep(ms) {
+	return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+prisma
+	.items({
+		where: {
+			owner: null,
+			section: {
+				project: {
+					NOT: {owner: null},
+				},
+			},
+		},
+	})
+	.$fragment(
+		gql`
+			fragment Owner on Item {
+				section {
+					project {
+						owner {
+							id
+						}
+					}
+				}
+			}
+		`,
+	)
+	.then((items) => {
+		items.map(async (item) => {
+			try {
+				const updatedItem = await prisma.updateItem({
+					where: {id: item.id},
+					data: {
+						owner: {connect: item.section.project.owner.id},
+					},
+				});
+
+				console.log(updatedItem.id, 'updated owner');
+			}
+			catch (e) {
+				console.log('oopsie', e);
+			}
+
+			await sleep(100);
+		});
+	});
