dbac0809f55174133c59539db1f0b29a4944e540
Alexis Launay <yorunohikage@gmail.com>
2019-03-20 17:31:50 +0100

    Fix removeCustomer typos

--- resolvers/removeCustomer.js
+++ resolvers/removeCustomer.js
@@ -4,18 +4,16 @@ const {NotFoundError} = require('../errors');
 const removeCustomer = async (parent, {id}, ctx) => {
 	const userId = getUserId(ctx);
 	const existingCustomer = await ctx.db.$exists.customer({
-		where: {
-			id,
-			serviceCompany: {
-				owner: {
-					id: userId,
-				},
+		id,
+		serviceCompany: {
+			owner: {
+				id: userId,
 			},
 		},
 	});
 
 	if (!existingCustomer) {
-		throw NotFoundError(`Customer '${id}' has not been found.`);
+		throw new NotFoundError(`Customer '${id}' has not been found.`);
 	}
 
 	return ctx.db.deleteCustomer({id});
