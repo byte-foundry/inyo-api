05f7b10e8b22ca3d6e4d4ec42d0af7c76ba57f12
Alexis Launay <yorunohikage@gmail.com>
2019-04-02 18:24:07 +0200

    Change postComment behavior and add test

---
+++ resolvers/__tests__/postComment.js
@@ -0,0 +1,84 @@
+import {postComment} from '../postComment';
+
+import {sendNewCommentEmail} from '../../emails/CommentEmail';
+
+jest.mock('../../utils');
+jest.mock('../../emails/CommentEmail');
+
+describe('postComment', () => {
+	it('should post a comment and notify customers', async () => {
+		const args = {
+			itemId: 'item-id',
+			comment: {
+				text: 'Mon commentaire',
+			},
+		};
+		const ctx = {
+			request: {
+				get: () => 'user-token',
+			},
+			db: {
+				items: () => ({
+					$fragment: () => [
+						{
+							id: 'item-id',
+							owner: {
+								firstName: 'Lance',
+								lastName: 'Free',
+								email: 'lance.free@test.com',
+							},
+							linkedCustomer: {
+								id: 'customer-task',
+								token: 'token-customer-task',
+								firstName: 'Jean',
+								lastName: 'Michel',
+								email: 'jeanmichel@customer-task.test',
+							},
+							section: {
+								id: 'section-id',
+								items: [],
+								project: {
+									id: 'project-id',
+									token: 'project-token',
+									notifyActivityToCustomer: true,
+									status: 'ONGOING',
+									customer: {
+										id: 'customer-project',
+										token: 'token-customer-project',
+										firstName: 'Bernard',
+										lastName: 'David',
+										email: 'bernarddavid@customer-project.test',
+									},
+								},
+							},
+						},
+					],
+				}),
+				updateItem: ({data}) => ({
+					id: 'item-id',
+					comments: [data.comments.create],
+				}),
+			},
+		};
+
+		const result = await postComment({}, args, ctx);
+
+		// project customer
+		expect(sendNewCommentEmail).toHaveBeenCalledWith(
+			expect.objectContaining({
+				email: 'bernarddavid@customer-project.test',
+				url: '/token-customer-project/tasks/item-id?projectId=project-id',
+			}),
+		);
+
+		// linked customer
+		expect(sendNewCommentEmail).toHaveBeenCalledWith(
+			expect.objectContaining({
+				email: 'jeanmichel@customer-task.test',
+				url: '/token-customer-task/tasks/item-id',
+			}),
+		);
+
+		expect(result.comments[0]).toMatchObject(args.comment);
+	});
+});
