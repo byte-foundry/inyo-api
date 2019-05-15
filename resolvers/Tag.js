090463264c8469b80be27291c6cf55b6ebb6ff34
FranzPoize <francois.poizat@gmail.com>
2019-05-15 15:03:26 +0200

    added items to Tag to list number of items

--- resolvers/Tag.js
+++ resolvers/Tag.js
@@ -3,6 +3,7 @@ const Tag = {
 	name: node => node.name,
 	colorBg: node => node.colorBg,
 	colorText: node => node.colorText,
+	items: (node, args, ctx) => ctx.db.tag({id: node.id}).items(),
 };
 
 module.exports = {
