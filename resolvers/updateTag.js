8a4eec57c291633fea62677356428707597a3a34
FranzPoize <francois.poizat@gmail.com>
2019-05-16 18:28:13 +0200

    taking into account that colors are not required

--- resolvers/updateTag.js
+++ resolvers/updateTag.js
@@ -17,13 +17,13 @@ const updateTag = async (parent, {id, ...tag}, ctx) => {
 		throw new NotFoundError(`Tag '${id}' has not been found.`);
 	}
 
-	if (!tag.colorBg.match(/#[0-9a-fA-F]{6}/)) {
+	if (tag.colorBg && !tag.colorBg.match(/#[0-9a-fA-F]{6}/)) {
 		throw new Error(
 			`tags background color must be a valid color not ${tag.color}`,
 		);
 	}
 
-	if (!tag.colorText.match(/#[0-9a-fA-F]{6}/)) {
+	if (tag.colorText && !tag.colorText.match(/#[0-9a-fA-F]{6}/)) {
 		throw new Error(`tags text color must be a valid color not ${tag.color}`);
 	}
 
