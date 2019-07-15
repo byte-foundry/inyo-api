e772b5af6cc6c0e17b2f10d6224c7ab655fb88f3
FranzPoize <francois.poizat@gmail.com>
2019-07-15 17:47:35 +0200

    added the possibility of having a userEvent triggering a notification

--- resolvers/Notification.js
+++ resolvers/Notification.js
@@ -1,27 +1,67 @@
 const Notification = {
 	id: node => node.id,
 	unread: node => node.unread,
-	from: (node, args, ctx) => ctx.db
-		.notification({id: node.id})
-		.customerEvent()
-		.customer(),
-	object: async (node, args, ctx) => {
-		const event = await ctx.db.notification({id: node.id}).customerEvent();
-		const {id, itemId, projectId} = event.metadata;
+	from: (node, args, ctx) => {
+		const customer = ctx.db
+			.notification({id: node.id})
+			.customerEvent()
+			.customer();
 
-		if (projectId) {
-			return ctx.db.project({id: projectId});
+		if (customer) {
+			return customer;
 		}
-		if (id || itemId) {
-			return ctx.db.item({id: itemId || id});
+
+		const user = ctx.db
+			.notification({id: node.id})
+			.userEvent()
+			.user();
+
+		if (user) {
+			return user;
+		}
+
+		return null;
+	},
+	object: async (node, args, ctx) => {
+		const customerEvent = await ctx.db
+			.notification({id: node.id})
+			.customerEvent();
+
+		if (customerEvent) {
+			const {id, itemId, projectId} = customerEvent.metadata;
+
+			if (projectId) {
+				return ctx.db.project({id: projectId});
+			}
+			if (id || itemId) {
+				return ctx.db.item({id: itemId || id});
+			}
+		}
+
+		const userEvent = await ctx.db.notification({id: node.id}).userEvent();
+
+		if (userEvent) {
+			return userEvent.metadata;
 		}
 
 		return null;
 	},
 	eventType: async (node, args, ctx) => {
-		const event = await ctx.db.notification({id: node.id}).customerEvent();
+		const customerEvent = await ctx.db
+			.notification({id: node.id})
+			.customerEvent();
 
-		return event.type;
+		if (customerEvent) {
+			return event.type;
+		}
+
+		const userEvent = await ctx.db.notification({id: node.id}).userEvent();
+
+		if (userEvent) {
+			return userEvent.type;
+		}
+
+		return null;
 	},
 	createdAt: node => node.createdAt,
 	updatedAt: node => node.updatedAt,
