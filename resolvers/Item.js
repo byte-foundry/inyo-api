fec1ec335d33c3f8a236b5fe8f72c4a8e6608a20
Alexis Launay <yorunohikage@gmail.com>
2019-08-02 18:08:19 +0200

    Add comment views update when comments is requested on Item

--- resolvers/Item.js
+++ resolvers/Item.js
@@ -1,5 +1,7 @@
 const {remindersSequences} = require('../emails/TaskEmail');
 
+const {ADMIN_TOKEN} = process.env;
+
 const Item = {
 	id: node => node.id,
 	name: node => node.name,
@@ -39,7 +41,30 @@ const Item = {
 	pendingUnit: node => node.pendingUnit,
 	unit: node => node.unit,
 	section: (node, args, ctx) => ctx.db.item({id: node.id}).section(),
-	comments: (node, args, ctx) => ctx.db.item({id: node.id}).comments(),
+	comments: async (node, args, ctx) => {
+		const comments = ctx.db.item({id: node.id}).comments();
+
+		if (ctx.token !== ADMIN_TOKEN) {
+			await Promise.all(
+				comments.map(comment => ctx.db.updateComment({
+					where: {id: comment.id},
+					data: {
+						views: {
+							create: ctx.token
+								? {
+									customer: {connect: {token: ctx.token}},
+									  }
+								: {
+									user: {connect: {id: ctx.userId}},
+									  },
+						},
+					},
+				})),
+			);
+		}
+
+		return comments;
+	},
 	vatRate: () => null,
 	status: node => node.status,
 	reviewer: node => (node.type === 'CUSTOMER' ? 'CUSTOMER' : 'USER'),
