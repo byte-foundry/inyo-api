97e2fd98f3fb1b1b7975bb020a174a450a13d55f
Alexis Launay <yorunohikage@gmail.com>
2019-09-27 10:53:42 +0200

    Add forgotten task validation email english version and prevent crash when the template is missing, falling back to the given id

--- emails/getTemplateId.js
+++ emails/getTemplateId.js
@@ -1,6 +1,22 @@
 const {templateTable} = require('../translations/templateTable');
 
 function getTemplateId(id, ctx) {
+	if (!templateTable[id]) {
+		console.log(
+			`Template ${id} seems to be missing from the template table list, falling back to the given id.`,
+		);
+		return id;
+	}
+
+	if (!templateTable[id][ctx.language]) {
+		console.log(
+			`[${
+				ctx.language
+			}] version of template ${id} seems to be missing from the template table list, falling back to the given id.`,
+		);
+		return id;
+	}
+
 	return templateTable[id][ctx.language];
 }
 
