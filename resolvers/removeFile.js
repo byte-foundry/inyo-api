ab31b5e5cab029e94e4b7fdbbce2aaa1077220b1
Alexis Launay <yorunohikage@gmail.com>
2019-11-06 17:08:24 +0100

    Add REMOVED_ATTACHMENT event and link UPLOADED_ATTACHMENT to project and task

--- resolvers/removeFile.js
+++ resolvers/removeFile.js
@@ -1,4 +1,42 @@
-const removeFile = (parent, {id}, ctx) => ctx.db.deleteFile({id});
+const gql = String.raw;
+
+const removeFile = async (parent, {id}, ctx) => {
+	const removedFile = await ctx.db.deleteFile({id}).$fragment(gql`
+		fragment RemovedFileTaskAndProject on File {
+			filename
+			linkedTask {
+				id
+				section {
+					project {
+						id
+					}
+				}
+			}
+			linkedProject {
+				id
+			}
+		}
+	`);
+
+	const project
+		= removedFile.linkedTask && removedFile.linkedTask.section
+			? removedFile.linkedTask.section.project
+			: removedFile.linkedProject;
+
+	await ctx.db.createUserEvent({
+		type: 'REMOVED_ATTACHMENT',
+		user: {
+			connect: {id: ctx.userId},
+		},
+		metadata: {
+			name: removedFile.filename,
+		},
+		item: removedFile.linkedTask && {connect: {id: removedFile.linkedTask.id}},
+		project: project && {connect: {id: project.id}},
+	});
+
+	return removedFile;
+};
 
 module.exports = {
 	removeFile,
