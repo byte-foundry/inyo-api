0e6cbda0bbd87e58d7d6a39ae1ce20ff48a56154
Alexis Launay <yorunohikage@gmail.com>
2019-11-12 18:55:21 +0100

    Fix finishItem and unfinishItem tests

--- resolvers/__tests__/unfinishItem.js
+++ resolvers/__tests__/unfinishItem.js
@@ -134,66 +134,6 @@ describe('unfinishItem', () => {
 		});
 	});
 
-	it('should not let a user reset a project customer item', async () => {
-		const args = {
-			id: 'item-id',
-		};
-		const ctx = {
-			request: {
-				get: () => 'user-token',
-			},
-			db: {
-				...db,
-				items: () => ({
-					$fragment: () => [
-						{
-							name: 'Mon item',
-							status: 'FINISHED',
-							type: 'CUSTOMER',
-							section: {
-								id: 'section-id',
-								project: {
-									id: 'project-id',
-									token: 'customer-token',
-									name: "C'est notre projeeet",
-									status: 'ONGOING',
-									sections: [
-										{
-											name: 'Ma section',
-											items: [
-												{
-													status: 'FINISHED',
-												},
-											],
-										},
-									],
-								},
-							},
-						},
-					],
-				}),
-				item: () => ({
-					$fragment: () => ({
-						section: {
-							items: [],
-							project: {
-								sections: [],
-							},
-						},
-					}),
-				}),
-				updateItem: ({data}) => ({
-					id: 'item-id',
-					...data,
-				}),
-			},
-		};
-
-		await expect(unfinishItem({}, args, ctx)).rejects.toThrow(
-			/cannot be resetted by the user/,
-		);
-	});
-
 	it('should not let a customer finish a project user item', async () => {
 		const args = {
 			id: 'item-id',
