c64eed7cfd62042899fcb337b1b1b337bb20a329
Francois Poizat <francois.poizat@gmail.com>
2019-12-09 19:28:51 +0100

    more content for invoice and content acquisition

--- emails/templates/contentAcquisition.js
+++ emails/templates/contentAcquisition.js
@@ -2,8 +2,943 @@ const templates = {
 	DELAY: {
 		en: {},
 		fr: {
-			subject: {},
-			content: {},
+			subject: {
+				object: 'value',
+				document: {
+					object: 'document',
+					data: {},
+					nodes: [
+						{
+							object: 'block',
+							type: 'paragraph',
+							data: {},
+							nodes: [
+								{
+									object: 'text',
+									text: '',
+									marks: [],
+								},
+								{
+									object: 'inline',
+									type: 'param',
+									data: {
+										param: {
+											id: 'ck3yjczq6a3kb0737h4yugmro',
+											name: 'project.name',
+											__typename: 'EmailParam',
+										},
+									},
+									nodes: [
+										{
+											object: 'text',
+											text: 'project.name',
+											marks: [],
+										},
+									],
+								},
+								{
+									object: 'text',
+									text: ' - ',
+									marks: [],
+								},
+								{
+									object: 'inline',
+									type: 'param',
+									data: {
+										param: {
+											id: 'ck3yjczmva3hj0737xv4dp273',
+											name: 'task.name',
+											__typename: 'EmailParam',
+										},
+									},
+									nodes: [
+										{
+											object: 'text',
+											text: 'task.name',
+											marks: [],
+										},
+									],
+								},
+								{
+									object: 'text',
+									text: '',
+									marks: [],
+								},
+							],
+						},
+					],
+				},
+			},
+			content: {
+				object: 'value',
+				document: {
+					object: 'document',
+					data: {},
+					nodes: [
+						{
+							object: 'block',
+							type: 'paragraph',
+							data: {},
+							nodes: [
+								{
+									object: 'text',
+									text: 'Bonjour ',
+									marks: [],
+								},
+								{
+									object: 'inline',
+									type: 'param',
+									data: {
+										param: {
+											id: 'ck3yjczoua3jb0737dte08a6u',
+											name: 'customer.fullname',
+											__typename: 'EmailParam',
+										},
+									},
+									nodes: [
+										{
+											object: 'text',
+											text: 'customer.fullname',
+											marks: [],
+										},
+									],
+								},
+								{
+									object: 'text',
+									text: ',',
+									marks: [],
+								},
+							],
+						},
+						{
+							object: 'block',
+							type: 'paragraph',
+							data: {},
+							nodes: [
+								{
+									object: 'text',
+									text: '',
+									marks: [],
+								},
+							],
+						},
+						{
+							object: 'block',
+							type: 'paragraph',
+							data: {},
+							nodes: [
+								{
+									object: 'text',
+									text: '',
+									marks: [],
+								},
+								{
+									object: 'inline',
+									type: 'param',
+									data: {
+										param: {
+											id: 'ck3yjczooa3j70737iag0x6kx',
+											name: 'customer.lastname',
+											__typename: 'EmailParam',
+										},
+									},
+									nodes: [
+										{
+											object: 'text',
+											text: 'customer.lastname',
+											marks: [],
+										},
+									],
+								},
+								{
+									object: 'text',
+									text:
+										' me charge de récupérer les contenus pour le projet en cours ',
+									marks: [],
+								},
+								{
+									object: 'inline',
+									type: 'param',
+									data: {
+										param: {
+											id: 'ck3yjczq6a3kb0737h4yugmro',
+											name: 'project.name',
+											__typename: 'EmailParam',
+										},
+									},
+									nodes: [
+										{
+											object: 'text',
+											text: 'project.name',
+											marks: [],
+										},
+									],
+								},
+								{
+									object: 'text',
+									text: '.',
+									marks: [],
+								},
+							],
+						},
+						{
+							object: 'block',
+							type: 'paragraph',
+							data: {},
+							nodes: [
+								{
+									object: 'text',
+									text:
+										'Merci de les envoyer en répondant à ce mail ou en les déposant au lien suivant :',
+									marks: [],
+								},
+							],
+						},
+						{
+							object: 'block',
+							type: 'paragraph',
+							data: {},
+							nodes: [
+								{
+									object: 'text',
+									text: '',
+									marks: [],
+								},
+							],
+						},
+						{
+							object: 'block',
+							type: 'paragraph',
+							data: {},
+							nodes: [
+								{
+									object: 'text',
+									text: '',
+									marks: [],
+								},
+								{
+									object: 'inline',
+									type: 'param',
+									data: {
+										param: {
+											id: 'ck3yjczn3a3hr0737qgsdg62p',
+											name: 'task.link',
+											__typename: 'EmailParam',
+										},
+									},
+									nodes: [
+										{
+											object: 'text',
+											text: 'task.link',
+											marks: [],
+										},
+									],
+								},
+								{
+									object: 'text',
+									text: '',
+									marks: [],
+								},
+							],
+						},
+						{
+							object: 'block',
+							type: 'paragraph',
+							data: {},
+							nodes: [
+								{
+									object: 'text',
+									text: '',
+									marks: [],
+								},
+							],
+						},
+						{
+							object: 'block',
+							type: 'paragraph',
+							data: {},
+							nodes: [
+								{
+									object: 'text',
+									text: 'Voici la liste des documents nécessaires :',
+									marks: [],
+								},
+							],
+						},
+						{
+							object: 'block',
+							type: 'paragraph',
+							data: {},
+							nodes: [
+								{
+									object: 'text',
+									text: '',
+									marks: [],
+								},
+							],
+						},
+						{
+							object: 'block',
+							type: 'paragraph',
+							data: {},
+							nodes: [
+								{
+									object: 'text',
+									text: '',
+									marks: [],
+								},
+							],
+						},
+						{
+							object: 'block',
+							type: 'paragraph',
+							data: {},
+							nodes: [
+								{
+									object: 'text',
+									text:
+										'Une fois les documents déposés, merci de cocher les cases correspondantes.',
+									marks: [],
+								},
+							],
+						},
+						{
+							object: 'block',
+							type: 'paragraph',
+							data: {},
+							nodes: [
+								{
+									object: 'text',
+									text:
+										"N'hésitez pas à revenir vers moi en cas de difficultés.",
+									marks: [],
+								},
+							],
+						},
+					],
+				},
+			},
+		},
+	},
+	FIRST: {
+		en: {},
+		fr: {
+			subject: {
+				object: 'value',
+				document: {
+					object: 'document',
+					data: {},
+					nodes: [
+						{
+							object: 'block',
+							type: 'paragraph',
+							data: {},
+							nodes: [
+								{
+									object: 'text',
+									text: '',
+									marks: [],
+								},
+								{
+									object: 'inline',
+									type: 'param',
+									data: {
+										param: {
+											id: 'ck3yjczq6a3kb0737h4yugmro',
+											name: 'project.name',
+											__typename: 'EmailParam',
+										},
+									},
+									nodes: [
+										{
+											object: 'text',
+											text: 'project.name',
+											marks: [],
+										},
+									],
+								},
+								{
+									object: 'text',
+									text: ' - ',
+									marks: [],
+								},
+								{
+									object: 'inline',
+									type: 'param',
+									data: {
+										param: {
+											id: 'ck3yjczmva3hj0737xv4dp273',
+											name: 'task.name',
+											__typename: 'EmailParam',
+										},
+									},
+									nodes: [
+										{
+											object: 'text',
+											text: 'task.name',
+											marks: [],
+										},
+									],
+								},
+								{
+									object: 'text',
+									text: '',
+									marks: [],
+								},
+							],
+						},
+					],
+				},
+			},
+			content: {
+				object: 'value',
+				document: {
+					object: 'document',
+					data: {},
+					nodes: [
+						{
+							object: 'block',
+							type: 'paragraph',
+							data: {},
+							nodes: [
+								{
+									object: 'text',
+									text: 'Bonjour ',
+									marks: [],
+								},
+								{
+									object: 'inline',
+									type: 'param',
+									data: {
+										param: {
+											id: 'ck3yjczoua3jb0737dte08a6u',
+											name: 'customer.fullname',
+											__typename: 'EmailParam',
+										},
+									},
+									nodes: [
+										{
+											object: 'text',
+											text: 'customer.fullname',
+											marks: [],
+										},
+									],
+								},
+								{
+									object: 'text',
+									text: ',',
+									marks: [],
+								},
+							],
+						},
+						{
+							object: 'block',
+							type: 'paragraph',
+							data: {},
+							nodes: [
+								{
+									object: 'text',
+									text: '',
+									marks: [],
+								},
+							],
+						},
+						{
+							object: 'block',
+							type: 'paragraph',
+							data: {},
+							nodes: [
+								{
+									object: 'text',
+									text:
+										'Sauf erreur de ma part, il manque encore des documents pour le projet en cours ',
+									marks: [],
+								},
+								{
+									object: 'inline',
+									type: 'param',
+									data: {
+										param: {
+											id: 'ck3yjczq6a3kb0737h4yugmro',
+											name: 'project.name',
+											__typename: 'EmailParam',
+										},
+									},
+									nodes: [
+										{
+											object: 'text',
+											text: 'project.name',
+											marks: [],
+										},
+									],
+								},
+								{
+									object: 'text',
+									text: '.',
+									marks: [],
+								},
+							],
+						},
+						{
+							object: 'block',
+							type: 'paragraph',
+							data: {},
+							nodes: [
+								{
+									object: 'text',
+									text:
+										"Avez-vous besoin de plus d'informations pour produire ces contenus?",
+									marks: [],
+								},
+							],
+						},
+						{
+							object: 'block',
+							type: 'paragraph',
+							data: {},
+							nodes: [
+								{
+									object: 'text',
+									text: '',
+									marks: [],
+								},
+							],
+						},
+						{
+							object: 'block',
+							type: 'paragraph',
+							data: {},
+							nodes: [
+								{
+									object: 'text',
+									text:
+										'Posez-nous vos questions ou envoyez ces contenus en réponse à ce mail ou au lien suivant :',
+									marks: [],
+								},
+							],
+						},
+						{
+							object: 'block',
+							type: 'paragraph',
+							data: {},
+							nodes: [
+								{
+									object: 'text',
+									text: '',
+									marks: [],
+								},
+							],
+						},
+						{
+							object: 'block',
+							type: 'paragraph',
+							data: {},
+							nodes: [
+								{
+									object: 'text',
+									text: '',
+									marks: [],
+								},
+								{
+									object: 'inline',
+									type: 'param',
+									data: {
+										param: {
+											id: 'ck3yjczmva3hj0737xv4dp273',
+											name: 'task.name',
+											__typename: 'EmailParam',
+										},
+									},
+									nodes: [
+										{
+											object: 'text',
+											text: 'task.name',
+											marks: [],
+										},
+									],
+								},
+								{
+									object: 'text',
+									text: '',
+									marks: [],
+								},
+							],
+						},
+						{
+							object: 'block',
+							type: 'paragraph',
+							data: {},
+							nodes: [
+								{
+									object: 'text',
+									text: '',
+									marks: [],
+								},
+							],
+						},
+						{
+							object: 'block',
+							type: 'paragraph',
+							data: {},
+							nodes: [
+								{
+									object: 'text',
+									text: 'Les documents encore manquants :',
+									marks: [],
+								},
+							],
+						},
+						{
+							object: 'block',
+							type: 'paragraph',
+							data: {},
+							nodes: [
+								{
+									object: 'text',
+									text: '',
+									marks: [],
+								},
+							],
+						},
+						{
+							object: 'block',
+							type: 'paragraph',
+							data: {},
+							nodes: [
+								{
+									object: 'text',
+									text: '',
+									marks: [],
+								},
+							],
+						},
+						{
+							object: 'block',
+							type: 'paragraph',
+							data: {},
+							nodes: [
+								{
+									object: 'text',
+									text:
+										'Une fois les documents déposés, merci de cocher les cases correspondantes.',
+									marks: [],
+								},
+							],
+						},
+						{
+							object: 'block',
+							type: 'paragraph',
+							data: {},
+							nodes: [
+								{
+									object: 'text',
+									text:
+										"N'hésitez pas à revenir vers moi en cas de difficultés, nous serons ravis de vous aider.",
+									marks: [],
+								},
+							],
+						},
+					],
+				},
+			},
+		},
+	},
+	SECOND: {
+		en: {},
+		fr: {
+			subject: {
+				object: 'value',
+				document: {
+					object: 'document',
+					data: {},
+					nodes: [
+						{
+							object: 'block',
+							type: 'paragraph',
+							data: {},
+							nodes: [
+								{
+									object: 'text',
+									text: '',
+									marks: [],
+								},
+								{
+									object: 'inline',
+									type: 'param',
+									data: {
+										param: {
+											id: 'ck3yjczq6a3kb0737h4yugmro',
+											name: 'project.name',
+											__typename: 'EmailParam',
+										},
+									},
+									nodes: [
+										{
+											object: 'text',
+											text: 'project.name',
+											marks: [],
+										},
+									],
+								},
+								{
+									object: 'text',
+									text: ' - ',
+									marks: [],
+								},
+								{
+									object: 'inline',
+									type: 'param',
+									data: {
+										param: {
+											id: 'ck3yjczmva3hj0737xv4dp273',
+											name: 'task.name',
+											__typename: 'EmailParam',
+										},
+									},
+									nodes: [
+										{
+											object: 'text',
+											text: 'task.name',
+											marks: [],
+										},
+									],
+								},
+								{
+									object: 'text',
+									text: '',
+									marks: [],
+								},
+							],
+						},
+					],
+				},
+			},
+			content: {
+				object: 'value',
+				document: {
+					object: 'document',
+					data: {},
+					nodes: [
+						{
+							object: 'block',
+							type: 'paragraph',
+							data: {},
+							nodes: [
+								{
+									object: 'text',
+									text: 'Bonjour ',
+									marks: [],
+								},
+								{
+									object: 'inline',
+									type: 'param',
+									data: {
+										param: {
+											id: 'ck3yjczoua3jb0737dte08a6u',
+											name: 'customer.fullname',
+											__typename: 'EmailParam',
+										},
+									},
+									nodes: [
+										{
+											object: 'text',
+											text: 'customer.fullname',
+											marks: [],
+										},
+									],
+								},
+								{
+									object: 'text',
+									text: ',',
+									marks: [],
+								},
+							],
+						},
+						{
+							object: 'block',
+							type: 'paragraph',
+							data: {},
+							nodes: [
+								{
+									object: 'text',
+									text: '',
+									marks: [],
+								},
+							],
+						},
+						{
+							object: 'block',
+							type: 'paragraph',
+							data: {},
+							nodes: [
+								{
+									object: 'text',
+									text:
+										"Je me permets de revenir vers vous car il semblerait qu'il manque encore des documents pour le projet en cours ",
+									marks: [],
+								},
+								{
+									object: 'inline',
+									type: 'param',
+									data: {
+										param: {
+											id: 'ck3yjczq6a3kb0737h4yugmro',
+											name: 'project.name',
+											__typename: 'EmailParam',
+										},
+									},
+									nodes: [
+										{
+											object: 'text',
+											text: 'project.name',
+											marks: [],
+										},
+									],
+								},
+								{
+									object: 'text',
+									text: '',
+									marks: [],
+								},
+							],
+						},
+						{
+							object: 'block',
+							type: 'paragraph',
+							data: {},
+							nodes: [
+								{
+									object: 'text',
+									text:
+										'Éprouvez-vous des difficultés pour produire ces contenus?',
+									marks: [],
+								},
+							],
+						},
+						{
+							object: 'block',
+							type: 'paragraph',
+							data: {},
+							nodes: [
+								{
+									object: 'text',
+									text: '',
+									marks: [],
+								},
+							],
+						},
+						{
+							object: 'block',
+							type: 'paragraph',
+							data: {},
+							nodes: [
+								{
+									object: 'text',
+									text:
+										'Posez-nous vos questions ou envoyez ces contenus en réponse à ce mail ou au lien suivant :',
+									marks: [],
+								},
+							],
+						},
+						{
+							object: 'block',
+							type: 'paragraph',
+							data: {},
+							nodes: [
+								{
+									object: 'text',
+									text: '',
+									marks: [],
+								},
+							],
+						},
+						{
+							object: 'block',
+							type: 'paragraph',
+							data: {},
+							nodes: [
+								{
+									object: 'text',
+									text: '',
+									marks: [],
+								},
+								{
+									object: 'inline',
+									type: 'param',
+									data: {
+										param: {
+											id: 'ck3yjczmva3hj0737xv4dp273',
+											name: 'task.name',
+											__typename: 'EmailParam',
+										},
+									},
+									nodes: [
+										{
+											object: 'text',
+											text: 'task.name',
+											marks: [],
+										},
+									],
+								},
+								{
+									object: 'text',
+									text: '',
+									marks: [],
+								},
+							],
+						},
+						{
+							object: 'block',
+							type: 'paragraph',
+							data: {},
+							nodes: [
+								{
+									object: 'text',
+									text: '',
+									marks: [],
+								},
+							],
+						},
+						{
+							object: 'block',
+							type: 'paragraph',
+							data: {},
+							nodes: [
+								{
+									object: 'text',
+									text: 'Pour information, les documents concernés :',
+									marks: [],
+								},
+							],
+						},
+						{
+							object: 'block',
+							type: 'paragraph',
+							data: {},
+							nodes: [
+								{
+									object: 'text',
+									text: '',
+									marks: [],
+								},
+							],
+						},
+						{
+							object: 'block',
+							type: 'paragraph',
+							data: {},
+							nodes: [
+								{
+									object: 'text',
+									text:
+										'Dans tous les cas, je reste à votre disposition par email si vous aviez la moindre question.',
+									marks: [],
+								},
+							],
+						},
+					],
+				},
+			},
 		},
 	},
 };
--- emails/templates/invoice.js
+++ emails/templates/invoice.js
@@ -1,5 +1,470 @@
 const templates = {
-	DELAY: {
+	INVOICE_DELAY: {
+		en: {},
+		fr: {
+			subject: {
+				object: 'value',
+				document: {
+					object: 'document',
+					data: {},
+					nodes: [
+						{
+							object: 'block',
+							type: 'paragraph',
+							data: {},
+							nodes: [
+								{
+									object: 'text',
+									text: 'Bonjour ',
+									marks: [],
+								},
+								{
+									object: 'inline',
+									type: 'param',
+									data: {
+										param: {
+											id: 'ck3yjczoua3jb0737dte08a6u',
+											name: 'customer.fullname',
+											__typename: 'EmailParam',
+										},
+									},
+									nodes: [
+										{
+											object: 'text',
+											text: 'customer.fullname',
+											marks: [],
+										},
+									],
+								},
+								{
+									object: 'text',
+									text: ',',
+									marks: [],
+								},
+							],
+						},
+						{
+							object: 'block',
+							type: 'paragraph',
+							data: {},
+							nodes: [
+								{
+									object: 'text',
+									text: '',
+									marks: [],
+								},
+							],
+						},
+						{
+							object: 'block',
+							type: 'paragraph',
+							data: {},
+							nodes: [
+								{
+									object: 'text',
+									text: '',
+									marks: [],
+								},
+								{
+									object: 'inline',
+									type: 'param',
+									data: {
+										param: {
+											id: 'ck3yjczpia3jv0737mhu8d5su',
+											name: 'user.fullname',
+											__typename: 'EmailParam',
+										},
+									},
+									nodes: [
+										{
+											object: 'text',
+											text: 'user.fullname',
+											marks: [],
+										},
+									],
+								},
+								{
+									object: 'text',
+									text:
+										" m'a demandé de vous faire parvenir notre facture en date du correspondant au projet que vous pouvez consulter au lien suivant:",
+									marks: [],
+								},
+							],
+						},
+						{
+							object: 'block',
+							type: 'paragraph',
+							data: {},
+							nodes: [
+								{
+									object: 'text',
+									text: '',
+									marks: [],
+								},
+							],
+						},
+						{
+							object: 'block',
+							type: 'paragraph',
+							data: {},
+							nodes: [
+								{
+									object: 'text',
+									text:
+										'La facture est téléchargeable directement à cette adresse:',
+									marks: [],
+								},
+							],
+						},
+						{
+							object: 'block',
+							type: 'paragraph',
+							data: {},
+							nodes: [
+								{
+									object: 'text',
+									text: '',
+									marks: [],
+								},
+							],
+						},
+						{
+							object: 'block',
+							type: 'paragraph',
+							data: {},
+							nodes: [
+								{
+									object: 'text',
+									text:
+										'Je vous serais reconnaissant de respecter le délai légal de 30 jours pour ce paiement.',
+									marks: [],
+								},
+							],
+						},
+						{
+							object: 'block',
+							type: 'paragraph',
+							data: {},
+							nodes: [
+								{
+									object: 'text',
+									text: '',
+									marks: [],
+								},
+							],
+						},
+						{
+							object: 'block',
+							type: 'paragraph',
+							data: {},
+							nodes: [
+								{
+									object: 'text',
+									text:
+										'Merci de revenir vers moi par retour de mail si vous aviez la moindre question.',
+									marks: [],
+								},
+							],
+						},
+						{
+							object: 'block',
+							type: 'paragraph',
+							data: {},
+							nodes: [
+								{
+									object: 'text',
+									text: '',
+									marks: [],
+								},
+							],
+						},
+						{
+							object: 'block',
+							type: 'paragraph',
+							data: {},
+							nodes: [
+								{
+									object: 'text',
+									text:
+										"Dans l'attente, nous vous souhaitons une agréable journée.",
+									marks: [],
+								},
+							],
+						},
+						{
+							object: 'block',
+							type: 'paragraph',
+							data: {},
+							nodes: [
+								{
+									object: 'text',
+									text: '',
+									marks: [],
+								},
+							],
+						},
+						{
+							object: 'block',
+							type: 'paragraph',
+							data: {},
+							nodes: [
+								{
+									object: 'text',
+									text: 'Cordialement,',
+									marks: [],
+								},
+							],
+						},
+					],
+				},
+			},
+			content: {
+				object: 'value',
+				document: {
+					object: 'document',
+					data: {},
+					nodes: [
+						{
+							object: 'block',
+							type: 'paragraph',
+							data: {},
+							nodes: [
+								{
+									object: 'text',
+									text: 'Bonjour ',
+									marks: [],
+								},
+								{
+									object: 'inline',
+									type: 'param',
+									data: {
+										param: {
+											id: 'ck3yjczoua3jb0737dte08a6u',
+											name: 'customer.fullname',
+											__typename: 'EmailParam',
+										},
+									},
+									nodes: [
+										{
+											object: 'text',
+											text: 'customer.fullname',
+											marks: [],
+										},
+									],
+								},
+								{
+									object: 'text',
+									text: ',',
+									marks: [],
+								},
+							],
+						},
+						{
+							object: 'block',
+							type: 'paragraph',
+							data: {},
+							nodes: [
+								{
+									object: 'text',
+									text: '',
+									marks: [],
+								},
+							],
+						},
+						{
+							object: 'block',
+							type: 'paragraph',
+							data: {},
+							nodes: [
+								{
+									object: 'text',
+									text: '',
+									marks: [],
+								},
+								{
+									object: 'inline',
+									type: 'param',
+									data: {
+										param: {
+											id: 'ck3yjczpia3jv0737mhu8d5su',
+											name: 'user.fullname',
+											__typename: 'EmailParam',
+										},
+									},
+									nodes: [
+										{
+											object: 'text',
+											text: 'user.fullname',
+											marks: [],
+										},
+									],
+								},
+								{
+									object: 'text',
+									text:
+										" m'a demandé de vous faire parvenir notre facture en date du correspondant au projet que vous pouvez consulter au lien suivant:",
+									marks: [],
+								},
+							],
+						},
+						{
+							object: 'block',
+							type: 'paragraph',
+							data: {},
+							nodes: [
+								{
+									object: 'text',
+									text: '',
+									marks: [],
+								},
+							],
+						},
+						{
+							object: 'block',
+							type: 'paragraph',
+							data: {},
+							nodes: [
+								{
+									object: 'text',
+									text:
+										'La facture est téléchargeable directement à cette adresse:',
+									marks: [],
+								},
+							],
+						},
+						{
+							object: 'block',
+							type: 'paragraph',
+							data: {},
+							nodes: [
+								{
+									object: 'text',
+									text: '',
+									marks: [],
+								},
+							],
+						},
+						{
+							object: 'block',
+							type: 'paragraph',
+							data: {},
+							nodes: [
+								{
+									object: 'text',
+									text:
+										'Je vous serais reconnaissant de respecter le délai légal de 30 jours pour ce paiement.',
+									marks: [],
+								},
+							],
+						},
+						{
+							object: 'block',
+							type: 'paragraph',
+							data: {},
+							nodes: [
+								{
+									object: 'text',
+									text: '',
+									marks: [],
+								},
+							],
+						},
+						{
+							object: 'block',
+							type: 'paragraph',
+							data: {},
+							nodes: [
+								{
+									object: 'text',
+									text:
+										'Merci de revenir vers moi par retour de mail si vous aviez la moindre question.',
+									marks: [],
+								},
+							],
+						},
+						{
+							object: 'block',
+							type: 'paragraph',
+							data: {},
+							nodes: [
+								{
+									object: 'text',
+									text: '',
+									marks: [],
+								},
+							],
+						},
+						{
+							object: 'block',
+							type: 'paragraph',
+							data: {},
+							nodes: [
+								{
+									object: 'text',
+									text:
+										"Dans l'attente, nous vous souhaitons une agréable journée.",
+									marks: [],
+								},
+							],
+						},
+						{
+							object: 'block',
+							type: 'paragraph',
+							data: {},
+							nodes: [
+								{
+									object: 'text',
+									text: '',
+									marks: [],
+								},
+							],
+						},
+						{
+							object: 'block',
+							type: 'paragraph',
+							data: {},
+							nodes: [
+								{
+									object: 'text',
+									text: 'Cordialement,',
+									marks: [],
+								},
+							],
+						},
+					],
+				},
+			},
+		},
+	},
+	INVOICE_FIRST: {
+		en: {},
+		fr: {
+			subject: {},
+			content: {},
+		},
+	},
+	INVOICE_SECOND: {
+		en: {},
+		fr: {
+			subject: {},
+			content: {},
+		},
+	},
+	INVOICE_THIRD: {
+		en: {},
+		fr: {
+			subject: {},
+			content: {},
+		},
+	},
+	INVOICE_FOURTH: {
+		en: {},
+		fr: {
+			subject: {},
+			content: {},
+		},
+	},
+	INVOICE_LAST: {
 		en: {},
 		fr: {
 			subject: {},
